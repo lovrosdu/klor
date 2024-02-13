2c7d23b837a5440e2f0af7758335036685f7207b
lovrosdu <lugovic@imada.sdu.dk>
2024-02-13 15:38:50 +0100

    Add socket transport and utilities

--- project.clj
+++ project.clj
@@ -3,9 +3,10 @@
   :url "http://github.com/lovrosdu/klor"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/core.async "1.6.681"]
+  :dependencies [[com.taoensso/nippy "3.3.0"]
                  [metabox/metabox "0.2.0"]
-                 [mvxcvi/puget "1.3.4"]]
+                 [mvxcvi/puget "1.3.4"]
+                 [org.clojure/clojure "1.11.1"]
+                 [org.clojure/core.async "1.6.681"]]
   :main klor.core
   :repl-options {:init-ns klor.core})
