e96f8817a044f66c38643116e97f33432e8d090e
lovrosdu <lugovic@imada.sdu.dk>
2024-09-16 14:43:00 +0200

    Use shorter variants for interop

--- src/klor/multi/sockets.clj
+++ src/klor/multi/sockets.clj
@@ -17,13 +17,13 @@
   false)
 
 (defn read-bc! ^ByteBuffer [^ReadableByteChannel bc ^ByteBuffer bb]
-  (while (not (zero? (. bb (remaining))))
-    (. bc (read bb)))
+  (while (not (zero? (.remaining bb)))
+    (.read bc bb))
   bb)
 
 (defn write-bc! ^ByteBuffer [^WritableByteChannel bc ^ByteBuffer bb]
-  (while (not (zero? (. bb (remaining))))
-    (. bc (write bb)))
+  (while (not (zero? (.remaining bb)))
+    (.write bc bb))
   bb)
 
 (defn socket-send [^SocketChannel sc value]
@@ -32,15 +32,15 @@
                     (.putLong (count bs))
                     (.flip)))
     (write-bc! sc (ByteBuffer/wrap bs)))
-  (when *log* (println (str (. sc (getRemoteAddress))) "<--" (pr-str value))))
+  (when *log* (println (str (.getRemoteAddress sc)) "<--" (pr-str value))))
 
 (defn socket-recv [^SocketChannel sc]
   (let [bb1 (ByteBuffer/allocate Long/BYTES)
-        n (.. (read-bc! sc bb1) (flip) (getLong))
+        n (.getLong (.flip (read-bc! sc bb1)))
         bb2 (ByteBuffer/allocate n)
-        bs (. (read-bc! sc bb2) (array))
+        bs (.array (read-bc! sc bb2))
         value (nippy/thaw bs)]
-    (when *log* (println (str (. sc (getRemoteAddress))) "-->" (pr-str value)))
+    (when *log* (println (str (.getRemoteAddress sc)) "-->" (pr-str value)))
     value))
 
 (defn wrap-sockets [config sockets & {:keys [log] :or {log :dynamic}}]
@@ -60,17 +60,17 @@
                 (.configureBlocking true)
                 (.setOption StandardSocketOptions/SO_REUSEADDR true)
                 (.bind (InetSocketAddress. (str ~host) (long ~port))))]
-     (try ~@body (finally (. ~sym (close))))))
+     (try ~@body (finally (.close ~sym)))))
 
 (defmacro with-accept [[ssc & syms] & body]
   (let [ssc# (gensym)]
     `(let [~ssc# ~ssc
-           ~@(mapcat identity (for [sym syms] [sym `(. ~ssc# (accept))]))]
-       (try ~@body (finally ~@(for [sym syms] `(. ~sym (close))))))))
+           ~@(mapcat identity (for [sym syms] [sym `(.accept ~ssc#)]))]
+       (try ~@body (finally ~@(for [sym syms] `(.close ~sym)))))))
 
 (defmacro with-client [[sym & {:keys [host port] :or {host "127.0.0.1"}}]
                        & body]
   `(let [~sym (doto (SocketChannel/open
                      (InetSocketAddress. (str ~host) (long ~port)))
                 (.configureBlocking true))]
-     (try ~@body (finally (. ~sym (close))))))
+     (try ~@body (finally (.close ~sym)))))
